91a6388f504d9238b1f69f73439e71617ba7eb45
wavesinaroom <juan@wavesinaroom.com>
2022-10-04 11:27:51 -0500

    [Feature] Add card date property using Date javascript object - must be tweaked when implementing date HTML object

--- src/card.js
+++ src/card.js
@@ -1,8 +1,9 @@
 import Product from "./product";
 
 export default class Card extends Product{
-  constructor(title, description){
+  constructor(title, description, dueDate){
     super(title);
     this.description = description;
+    this.dueDate = new Date(dueDate); //use it when inputting from html https://stackoverflow.com/questions/28760254/assign-javascript-date-to-html5-datetime-local-input
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import _ from 'lodash';
 import './style.css';
 import Card from './card.js';
 
-let card = new Card('My card','My card description');
+let card = new Card('My card','My card description','2020-05-12');
 console.log(card.title);
 console.log(card.description);
+console.log(card.dueDate);
+let time = new Date();
+console.time();
+console.log(time);
