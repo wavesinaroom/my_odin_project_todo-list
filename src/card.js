e469a7b71c343f6f5f0f4cfc85b07f4f65e2e69b
wavesinaroom <juan@wavesinaroom.com>
2022-10-04 12:09:23 -0500

    [Feature] Create and test card notes property

--- src/card.js
+++ src/card.js
@@ -12,10 +12,11 @@ export default class Card extends Product{
   static get PRIORITY(){
     return PRIORITY;
   }
-  constructor(title,description,dueDate, priority){
+  constructor(title,description,dueDate, priority, notes){
     super(title);
     this.description = description;
     this.dueDate = new Date(dueDate); //use it when inputting from html https://stackoverflow.com/questions/28760254/assign-javascript-date-to-html5-datetime-local-input
     this.priority = priority;
+    this.notes = notes;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import './style.css';
 import Card from './card.js';
 
-let card = new Card('My card','My card description','2020-05-12',Card.PRIORITY.HIGH);
+let card = new Card('My card','My card description','2020-05-12',Card.PRIORITY.HIGH, 'I\'m a very long card note');
 console.log(card.title);
 console.log(card.description);
 console.log(card.dueDate);
 console.log(card.priority);
+console.log(card.notes);
