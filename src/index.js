29827bc30226924d905f88fe9f2a18996385a8c9
wavesinaroom <juan@wavesinaroom.com>
2022-10-06 10:37:59 -0500

    [Task] Test card class fully

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import List from './list.js';
 import Project from './project.js';
 
 
+let project = new Project('Test');
+let list = new List('to-do');
+let card = new Card("My card","testing card","2022-10-7", Card.PRIORITY.DEFAULT, "Test passed", list.title)
 
-
+let cardClone = Card.clone(card);
+console.dir(card);
+console.dir(cardClone);
+card = null;
+console.dir(card);
 
 
