45aac8a21d0382f3721c3627f7a815cfe1dc21b6
wavesinaroom <juan@wavesinaroom.com>
2022-10-12 11:18:09 -0500

    [Feature] Implement checklistItem factory function

--- src/card.js
+++ src/card.js
@@ -10,6 +10,15 @@ const STATUS={
   INPROGRESS: 'InProgress'
 };
 
+function checklistItem(){
+  return {
+    check : false,
+    description : "",
+   removeChecklistItem: function (){
+      this = null; 
+    }
+  };
+}
 export default class Card {
 
   static get PRIORITY(){
@@ -31,7 +40,7 @@ export default class Card {
     this.dueDate = null; //use this when inputting from html https://stackoverflow.com/questions/28760254/assign-javascript-date-to-html5-datetime-local-input
     this.priority = priority; 
     this.notes = notes; 
-    this.checklist = new Map();
+    this.checklist = [];
     this.listName = listName; 
     this.completion = STATUS.INPROGRESS;
 
@@ -39,37 +48,49 @@ export default class Card {
       let innerHTML = ["<textarea>", this.title, "</textarea>",
                       "<textarea>", this.description, "</textarea>",
                       "<input type='date'>", this.dueDate, "</input>",
+                      "<section id='"+this.tag+"-checklist'></section>",
                       "<select name='priority'>",
                       "<option value='"+PRIORITY.LOW+"'>Low</option>",
                       "<option value='"+PRIORITY.MID+"'>Medium</option>",
                       "<option value='"+PRIORITY.HIGH+"'>High</option>",
                       "</select>",
-                      "<textarea>", this.notes, "</textarea>",
                       "<select name='status'>",
                       "<option value='"+STATUS.DONE+"'>Done</option>'", 
                       "<option value='"+STATUS.INPROGRESS+"'>In progress</option>'", 
                       "</select>",
-                      "<button id='save'>Save</button>"].join("");
+                      "<div>",
+                      "<button id='add-checklist'>Add checklist</button>",
+                      "<button id='remove-checklist'>Remove checklist</button>",
+                      "</div>",
+                      "<textarea>", this.notes, "</textarea>",
+                      "<button id='save-button'>Save</button>"].join("");
       document.getElementById(this.tag).innerHTML = innerHTML;
     }
 
-    this.saveChanges = function(){
-      this.title = document.querySelector('#'+this.tag+' :nth-child(1)').value;
-      this.description = document.querySelector('#'+this.tag+' :nth-child(2)').value;
-      this.dueDate = document.querySelector('#'+this.tag+' :nth-child(3)').value;
-      this.priority = document.querySelector('#'+this.tag+' :nth-child(4)').value;
-      this.notes = document.querySelector('#'+this.tag+' :nth-child(5)').value;
-      this.completion = document.querySelector('#'+this.tag+' :nth-child(6)').value;
-       
-      console.log(this.completion);
-    }
 
     let div = document.createElement('div');
     div.id = this.tag; 
     div.className = 'card';
     document.body.appendChild(div);
     this.render();
-    document.getElementById('save').addEventListener('click', ()=>{this.saveChanges();});
+
+    document.getElementById('save-button').addEventListener('click', ()=>{this.saveChanges();});
+    document.getElementById('add-checklist').addEventListener('click', ()=>{this.addChecklistItem();});
+    document.getElementById('remove-checklist').addEventListener('click', ()=>{this.removeChecklistItem();});
 
   }
+    addChecklistItem(){
+     let item = checklistItem();
+     this.checklist.push(item); 
+    } 
+
+    saveChanges = function(){
+      //Find out a way to use the onfocusout event to save changes in fields. 
+      this.title = document.querySelector('#'+this.tag+' :nth-child(1)').value;
+      this.description = document.querySelector('#'+this.tag+' :nth-child(2)').value;
+      this.dueDate = document.querySelector('#'+this.tag+' :nth-child(3)').value;
+      this.priority = document.querySelector('#'+this.tag+' :nth-child(4)').value;
+      this.completion = document.querySelector('#'+this.tag+' :nth-child(5)').value;
+      this.notes = document.querySelector('#'+this.tag+' :nth-child(7)').value;
+    }
 }
