d679e89f8300ff67c916842b27be5894a2cad7ec
wavesinaroom <juan@wavesinaroom.com>
2022-10-26 10:40:00 -0500

    [Task] Clean up clutter lines at the end of container class

--- src/container.js
+++ src/container.js
@@ -28,7 +28,4 @@ export default class Container extends Component{
     document.getElementById(id).parentNode.removeChild(document.getElementById(id)); 
     });    
   }
-
-  removeChild(id){
-  }
 }
