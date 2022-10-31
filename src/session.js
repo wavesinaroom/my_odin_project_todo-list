f3359dd5c8d8e012af6cd2ebd3d7fc6ff54e793b
wavesinaroom <juan@wavesinaroom.com>
2022-10-31 12:41:46 -0500

    [Fix] Restore this.div in session.js

--- src/session.js
+++ src/session.js
@@ -7,13 +7,13 @@ export default class Session extends Container{
     this.username = username;
     this.child = new Project();
     this.type = 'session';
-    this.innerHTML = ["<div id='"+this.username+"'-session data-checklist='' data-card='' data-list='' data-project''>",
+    this.innerHTML = ["<div id='"+this.username+"-session' data-checklist='' data-card='' data-list='' data-project''>",
                 "<H1>Call it a day!</H1>",
                 "<button id='session-add-button'>Add Project</button>",
                 "<button id='logout'>Log out</button>",
                 "</div>"].join("");  
-    document.body.innerHTML=this.innerHTML;
-
+    this.div.innerHTML = this.innerHTML
+    document.body.append(this.div);
     document.getElementById('session-add-button').addEventListener('click', ()=>{this.addChild(Project.getId());
     ++Project.count;});
   }
